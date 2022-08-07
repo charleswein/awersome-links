5544ce6037f651f50aba5f35df1b071c95e7017c
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-08-07 22:27:44 +0400

    fix: cors issues #3

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,15 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { URL } from 'url';
+
+const getHost = () => {
+  if(process.env.NODE_ENV !== 'production') {
+    return 'http://localhost:3000'
+  }
+  return 'https://awesome-links-sigma.vercel.app'
+}
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: `${getHost()}/api/graphql`,
   cache: new InMemoryCache(),
 })
 
