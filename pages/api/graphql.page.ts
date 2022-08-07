10923ded9a346129814b5a62fa714033d6abf2b4
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-08-07 20:19:01 +0400

    fix: fix cors problem

--- pages/api/graphql.page.ts
+++ pages/api/graphql.page.ts
@@ -1,15 +1,23 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { typeDefs } from '../../graphql/schema'
 import { resolvers } from '../../graphql/resolvers'
-import Cors from 'micro-cors'
 import { createContext } from '../../graphql/context';
-const cors = Cors()
+import { NextApiRequest, NextApiResponse } from "next";
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers, context: createContext })
 
 const startServer = apolloServer.start()
 
-export default cors(async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   if (req.method === 'OPTIONS') {
     res.end()
     return false
@@ -19,7 +27,7 @@ export default cors(async function handler(req, res) {
   await apolloServer.createHandler({
     path: '/api/graphql',
   })(req, res)
-})
+}
 
 export const config = {
   api: {
