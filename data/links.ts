d121a4cf5637ca75d1542342b69ac2858bb93e32
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-08-07 15:57:12 +0400

    chore: add tailwind css and seed first data to DB

---
+++ data/links.ts
@@ -0,0 +1,10 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: "8a9020b2-363b-4a4f-ad26-d6d55b51bbaf",
+    imageUrl: "https://nextjs.org/static/twitter-cards/home.jpg",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+]
